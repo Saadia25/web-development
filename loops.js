5e1a013e2125107758c89dbd4ecf8a2cb8fe1e6e
Saadia25 <saadiayousaf2520@gmail.com>
2025-02-24 17:03:31 +0500

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,36 @@
+for (let i = 0; i < 15; i++) {
+  console.log("hello");
+}
+
+
+for (let i = 15; i > 0; i--) {
+  console.log("for loop");
+}
+
+// for (let i = 1; i <= 15; i++) {
+//   console.log(i);
+// }
+
+// for (let i = 15; i > 0; i--) {
+//   console.log(i);
+// }
+
+// let fruites = ["banana", "apple", "cherry"];
+// for (let i = 0; i < fruites.length; i++) {
+//   console.log(`${fruites[i]} is better than other`);
+// }
+
+let i = 0;
+while (i < 5) {
+  console.log("hello");
+  i++;
+}
+
+let number=[100,100,63,963,852,1200,8500,980]
+let sum=0;
+for(i=0; i<number.length; i++)
+{
+    sum=sum+number[i];
+}
+console.log(sum);
+
